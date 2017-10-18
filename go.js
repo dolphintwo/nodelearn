ce5cd027326c06476484be8661eb787d0cd296f2
dolphintwo <dtdinghui@126.com>
2017-10-18 17:34:15 +0800

    1734

--- go.js
+++ go.js
@@ -2,4 +2,17 @@
 
 var fs = require('fs');
 
-var rs = fs.createReadStream('stream_sample.txt', { start: 1, end: 20, autoClose: true })
+var rs = fs.createReadStream('stream_sample.txt', { start: 0, end: 100, encoding: 'utf-8' });
+
+rs.on('data', function(chunk) {
+    console.log('DATA:');
+    console.log(chunk);
+});
+
+rs.on('end', function() {
+    console.log('END');
+});
+
+rs.on('error', function(err) {
+    console.log('ERROR:' + err);
+});
